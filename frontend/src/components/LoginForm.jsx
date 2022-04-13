c6afcb006ea43858e6dc1e236b19585d5edaf556
norbert <norbert.madauss@gmail.com>
2022-04-13 09:31:07 +0200

    fixed login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -7,14 +7,24 @@ function LoginForm() {
   const { t } = useTranslation();
   return (
     <form id="login-form">
-      <TextField id="standard-basic" label="E-Mail" variant="standard" />
-      <TextField id="standard-basic" label="Password" variant="standard" />
+      <TextField
+        id="standard-basic"
+        label="E-Mail"
+        variant="standard"
+        type="email"
+      />
+      <TextField
+        id="standard-basic"
+        label="Password"
+        variant="standard"
+        type="password"
+      />
       <p>
-        <a href="#">{t("login-forgot-password")}</a>
+        <a href="/">{t("login-forgot-password")}</a>
       </p>
       <Button variant="contained">{t("login-button")}</Button>
       <p>
-        {t("login-no-account-text")} <a href="#">{t("sign-up-button")}</a>
+        {t("login-no-account-text")} <a href="/">{t("sign-up-button")}</a>
       </p>
     </form>
   );
