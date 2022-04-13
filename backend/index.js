c4fb16685ff10481762fe619682a65c157b50753
Max Dahl <github@daagur.de>
2022-04-13 16:38:55 +0200

    implmented user authenification and authorization api endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-// const db = require("./utils/db");
+const db = require("./src/models");
 const routes = require("./src/routes");
 
 const app = express();
@@ -14,21 +14,38 @@ Object.keys(routes).forEach((route) => {
   app.use(`/api/${route}`, routes[route]);
 });
 
-const port = process.env.APP_PORT || 5000;
-app.listen(port, () => {
-  // eslint-disable-next-line no-restricted-syntax
-  console.log(`Server started on port ${port}`);
-});
+async function startServer() {
+  const port = process.env.APP_PORT || 5000;
+  app.listen(port, () => {
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`Server started on port ${port}`);
+  });
+}
 
-// db.sync()
-//   .then((result) => {
-//     console.log("Databse synced");
-//     const port = process.env.PORT || 3000;
-//     app.listen(port, () => {
-//       console.log(`Server started on port ${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error(err);
-//     process.exit(-1);
-//   });
+async function checkDB() {
+  try {
+    await db.sequelize.authenticate();
+
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`DB connected`);
+  } catch (err) {
+    console.error("DB CONNECTION FAILED", err);
+    throw err;
+  }
+}
+
+async function syncDB() {
+  try {
+    await db.sequelize.sync({ force: true });
+
+    // eslint-disable-next-line no-restricted-syntax
+    console.log(`DB synced`);
+  } catch (err) {
+    console.error("DB CONNECTION FAILED", err);
+    throw err;
+  }
+}
+
+startServer();
+checkDB();
+syncDB();
