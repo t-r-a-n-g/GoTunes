4dc167f4abfb764aeac2554359394477db007d3e
Trang <ttphan.contact@gmail.com>
2022-04-13 09:15:43 +0200

    Created RegistrationForm Component (no css)

--- frontend/src/pages/Authentification.jsx
+++ frontend/src/pages/Authentification.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LoginForm from "@components/LoginForm";
+import RegistrationForm from "@components/RegistrationForm";
 
 export default function Authentification() {
   return (
     <div>
       <h1>Logo Placeholder</h1> <LoginForm />
+      <RegistrationForm />
     </div>
   );
 }
