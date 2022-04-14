c3c8469c3189e589aa83f2a39b5e18b418174bda
Trang <ttphan.contact@gmail.com>
2022-04-14 16:25:06 +0200

    RegistrationForm: finished API call and response actions

--- frontend/src/components/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import Button from "@mui/material/Button";
 /* import Snackbar from "@mui/material/Snackbar";
 import Alert from '@mui/material/Alert'; */
 import TextField from "@mui/material/TextField";
 import { useTranslation } from "react-i18next";
-/* import axios from "axios"; */
-/* import { loginEndpoint } from "./API"; */
+import axios from "axios";
+import { registerEndpoint } from "./API";
 import "./RegistrationForm.css";
 
 export default function RegistrationForm() {
@@ -13,13 +13,13 @@ export default function RegistrationForm() {
 
   // Storing user input in variables using React state
   const [email, setEmail] = useState("");
-  /*  const [emailConfirm, setEmailConfirm] = useState(""); */
-  const [userName, setUserName] = useState("");
+  const [emailConfirm, setEmailConfirm] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  /*  const [passwordConfirm, setPasswordConfirm] = useState(""); */
+  const [passwordConfirm, setPasswordConfirm] = useState("");
 
   // Checking if user provides correct (matching) emails and passwords
-  /* function isSameEmail() {
+  function isSameEmail() {
     if (email === emailConfirm) {
       return true;
     }
@@ -32,20 +32,28 @@ export default function RegistrationForm() {
     }
     return false;
   }
- */
+
   // putting correct email, password and username in object to send it to API
   const userRegisterData = {};
+  userRegisterData.username = username;
   userRegisterData.email = email;
   userRegisterData.password = password;
-  userRegisterData.userName = userName;
 
-  // POST REQUEST TO API (after clicking button)
-  // TO DO: change console.log to something more useful
-  /* function handleSignUp() {
-    axios
-      .post(loginEndpoint, userRegisterData)
-      .then((response) => console.warn(response));
-  } */
+  // POST REQUEST TO API on SignUp Button, only possible if emails and passwords match
+  // update state of status in response and error response
+  const [status, setStatus] = useState("");
+  const handleSignUp = useCallback(() => {
+    if (isSameEmail && isSamePassword) {
+      axios
+        .post(registerEndpoint, userRegisterData)
+        .then((response) => {
+          setStatus(response.status);
+        })
+        .catch((error) => {
+          setStatus(error.response.status);
+        });
+    }
+  });
 
   return (
     <div>
@@ -55,6 +63,7 @@ export default function RegistrationForm() {
           label="E-Mail"
           variant="standard"
           type="email"
+          required
           onChange={(e) => setEmail(e.target.value)}
         />
 
@@ -63,21 +72,30 @@ export default function RegistrationForm() {
           label="Confirm E-Mail"
           variant="standard"
           type="email"
-          /* onChange={(e) => setEmailConfirm(e.target.value)} */
+          required
+          onChange={(e) => setEmailConfirm(e.target.value)}
         />
         {/* Error Message below: just testing, not final */}
-        {/* {isSameEmail() ? <p /> : <p>E-Mails don't match!</p>} */}
+        {isSameEmail() ? (
+          <p />
+        ) : (
+          <p className="error-message">
+            {t("registration-emails-not-matching")}
+          </p>
+        )}
         <TextField
           id="standard-basic"
           label="Choose Your User Name"
           variant="standard"
-          onChange={(e) => setUserName(e.target.value)}
+          required
+          onChange={(e) => setUsername(e.target.value)}
         />
         <TextField
           type="password"
           id="standard-basic"
           label="Password"
           variant="standard"
+          required
           onChange={(e) => setPassword(e.target.value)}
         />
         <TextField
@@ -85,9 +103,20 @@ export default function RegistrationForm() {
           id="standard-basic"
           label="Confirm Password"
           variant="standard"
-          /* onChange={(e) => setPasswordConfirm(e.target.value)} */
+          required
+          onChange={(e) => setPasswordConfirm(e.target.value)}
         />
-        <Button variant="contained" /* onClick={handleSignUp} */>
+        {isSamePassword() ? (
+          <p />
+        ) : (
+          <p className="error-message">
+            {t("registration-password-not-matching")}
+          </p>
+        )}
+        {status === 400 ? <p>{t("registration-false-email-format")}</p> : ""}
+        {status === 409 ? <p>{t("registration-acc-already-exists")}</p> : ""}
+        {status === 500 ? <p>{t("registration-internal-server-error")}</p> : ""}
+        <Button variant="contained" onClick={handleSignUp}>
           {t("sign-up-button")}
         </Button>
         <p>
