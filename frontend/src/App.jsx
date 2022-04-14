1af4ab36e428cced57a7e9fb27b6a3263dc45721
norbert <norbert.madauss@gmail.com>
2022-04-14 13:59:29 +0200

    added theme.jsx for global custoization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,15 @@
 import React, { Suspense } from "react";
-import Home from "@pages/Home";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Home from "./pages/Home";
+import themeGlobal from "./theme";
 import "./App.css";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      main: "#EE4540",
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: "#801336",
-    },
-  },
-});
 
 function App() {
   return (
     <Suspense fallback="loading">
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={themeGlobal}>
+        <CssBaseline />
         <div className="App">
           <Home />
         </div>
