0dc8d0c3f9a25819f8075f9624336e7661f06d4f
norbert <norbert.madauss@gmail.com>
2022-04-14 10:48:25 +0200

    update loginForm

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -3,27 +3,65 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { useTranslation } from "react-i18next";
 import "./LoginForm.css";
+import axios from "axios";
 
 function LoginForm() {
   const { t } = useTranslation();
+
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [emailInvalid, setEmailInvalid] = React.useState(false);
+
+  const userLoginData = {};
+  userLoginData.email = email;
+  userLoginData.password = password;
+
+  function checkEmail() {
+    if (email.match(/@/)) {
+      setEmailInvalid(false);
+      return true;
+    } else {
+      setEmailInvalid(true);
+      return false;
+    }
+  }
+
+  function handleLoginRequest() {
+    if (checkEmail()) {
+      axios
+        .post("http://localhost:5000/api/auth/login", userLoginData)
+        .then((response) => {
+          localStorage.setItem("userToken", response.data.token);
+        });
+    }
+  }
+
   return (
     <form id="login-form">
       <TextField
+        error={emailInvalid === true}
+        helperText={
+          emailInvalid === true ? t("login-invalid-email-message") : null
+        }
         id="standard-basic"
         label="E-Mail"
         variant="standard"
         type="email"
+        onChange={(e) => setEmail(e.target.value)}
       />
       <TextField
         id="standard-basic"
         label="Password"
         variant="standard"
         type="password"
+        onChange={(e) => setPassword(e.target.value)}
       />
       <p>
         <a href="/">{t("login-forgot-password")}</a>
       </p>
-      <Button variant="contained" id="button">{t("login-button")}</Button>
+      <Button variant="contained" id="button" onClick={handleLoginRequest}>
+        {t("login-button")}
+      </Button>
       <p>
         {t("login-no-account-text")} <a href="/">{t("sign-up-button")}</a>
       </p>
