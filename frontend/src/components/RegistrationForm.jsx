603f552fbe80f0e6185e73f39098b79f132016ce
Trang <ttphan.contact@gmail.com>
2022-04-20 14:51:48 +0200

    RegForm: edited error messages

--- frontend/src/components/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm.jsx
@@ -4,14 +4,16 @@ import TextField from "@mui/material/TextField";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import validator from "validator";
+/* import { useNavigate } from "react-router-dom"; */
 import { registerEndpoint } from "./API";
 import "./RegistrationForm.css";
 
 // TO DO: ROUTING TO ANOTHER PAGE AFTER SUCCESSFUL REGISTRATION
-// IF USER SHOULD BE ALREADY LOGGED IN AFTER SIGN UP THEN USERTOKEN NEEDS TO BE STORED IN LOCALSTORAGE
+// TO DO: make all input fields mandatory
 
 export default function RegistrationForm() {
   const { t } = useTranslation();
+  /* const navigate = useNavigate(); */
 
   // Storing user input in variables using React state
   const [email, setEmail] = useState("");
@@ -39,25 +41,49 @@ export default function RegistrationForm() {
   // only possible if emails and passwords match and if email format is valid
   // update state of status in response and error response
   const [status, setStatus] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  /* const [inputFieldsFilledOut, setInputFieldsFilledOut] = useState(false); */
   const [emailValid, setEmailValid] = useState("");
 
+  // Function to be executed when clicking SignUp Button
   const handleSignUp = useCallback(() => {
+    // check for valid email format
     if (validator.isEmail(email)) {
       setEmailValid(true);
     } else setEmailValid(false);
+    // check for empty input fields
+    /* still needs to be done */
+
+    // make API request when all checks passed
     if (isSameEmail && isSamePassword && emailValid) {
       axios
         .post(registerEndpoint, userRegisterData)
         .then((response) => {
-          console.log(response);
+          /* console.log(response); */
           setStatus(response.status);
           localStorage.setItem("userToken", response.data.token);
         })
         .catch((error) => {
           setStatus(error.response.status);
+          setErrorMsg(error.response.data.errors);
         });
     }
   });
+
+  // pulling out error messages from error code 409
+  let errorMsgUsername = "";
+  let errorMsgEmail = "";
+  if (errorMsg.length === 2) {
+    errorMsgUsername = errorMsg[0].username;
+    errorMsgEmail = errorMsg[1].email;
+  } else if (errorMsg.length === 1) {
+    if (errorMsg[0].username) {
+      errorMsgUsername = errorMsg[0].username;
+    }
+    if (errorMsg[0].email) {
+      errorMsgEmail = errorMsg[0].email;
+    }
+  }
 
   return (
     <div>
@@ -79,14 +105,17 @@ export default function RegistrationForm() {
           required
           onChange={(e) => setEmailConfirm(e.target.value)}
         />
+
         {isSameEmail() ? (
-          <p />
+          ""
         ) : (
           <p className="error-message">
             {t("registration-emails-not-matching")}
           </p>
         )}
         {emailValid === false ? <p>{t("registration-invalid-email")}</p> : ""}
+        {status === 409 ? <p>{t(errorMsgEmail)}</p> : ""}
+        {status === 400 ? <p>{t("registration-false-email-format")}</p> : ""}
         <TextField
           id="registration-username"
           label="Choose Your User Name"
@@ -94,6 +123,7 @@ export default function RegistrationForm() {
           required
           onChange={(e) => setUsername(e.target.value)}
         />
+        {status === 409 ? <p>{t(errorMsgUsername)}</p> : ""}
         <TextField
           type="password"
           id="registration-password"
@@ -117,8 +147,6 @@ export default function RegistrationForm() {
             {t("registration-password-not-matching")}
           </p>
         )}
-        {status === 400 ? <p>{t("registration-false-email-format")}</p> : ""}
-        {status === 409 ? <p>{t("registration-acc-already-exists")}</p> : ""}
         {status === 500 ? <p>{t("registration-internal-server-error")}</p> : ""}
         <Button variant="contained" onClick={handleSignUp}>
           {t("sign-up-button")}
