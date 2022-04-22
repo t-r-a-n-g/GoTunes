543c3a18c652fdd6c6cacccc7e5119db83c9eb78
Trang <ttphan.contact@gmail.com>
2022-04-22 11:56:37 +0200

    edited Musicplayer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import React, { Suspense } from "react";
+import React, { useState, Suspense } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import "./App.css";
-import SearchGenre from "@pages/SearchGenre";
-import SearchResult from "@pages/SearchResult";
+import SearchGenre from "./pages/SearchGenre";
+import SearchResult from "./pages/SearchResult";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import UserProfil from "./pages/UserProfil";
@@ -13,10 +13,30 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import MusicPlayerTestPage from "./pages/MusicPlayerTestPage";
 import themeGlobal from "./theme";
+import MusicPlayer from "./components/MusicPlayer";
 
 function App() {
   // add function to check if user is auth, and return a boolean into "user"
   const user = true;
+
+  // state for songQueue
+  const [songQueue, setSongQueue] = useState([]);
+
+  // This will become a variable for the player.. To connect the player with buttons (our own controls),
+  // we can then call its methods, such as audioInstance.pause().
+  const [audioInstance, setAudioInstance] = useState(null);
+
+  // config options for the player (audioLists is current songQueue)
+  const playerOptions = {
+    audioLists: songQueue,
+    mode: "full",
+    showDownload: false,
+    theme: "dark",
+    getAudioInstance(instance) {
+      setAudioInstance(instance);
+    },
+  };
+
   return (
     <Router>
       <Suspense fallback="loading">
@@ -24,12 +44,19 @@ function App() {
           <CssBaseline />
 
           <div className="App">
+            <MusicPlayer
+              playerOptions={playerOptions}
+              audioInstance={audioInstance}
+            />
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/registration" element={<Registration />} />
               {/* Search Route needs to be moved into Protected Route later on */}
-              <Route path="/search" element={<Search />} />
+              <Route
+                path="/search"
+                element={<Search setSongQueue={setSongQueue} />}
+              />
               <Route
                 path="/MusicPlayerTestPage"
                 element={<MusicPlayerTestPage />}
--- frontend/src/components/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer.jsx
@@ -1,33 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
 import PropTypes from "prop-types";
 
-function MusicPlayer({ playlist }) {
-  // This will become a variable for the player.. To connect the player with buttons (our own controls),
-  // we can then call its methods, such as audioInstance.pause().
-  const [audioInstance, setAudioInstance] = useState(null);
-
-  const options = {
-    audioLists: playlist,
-    mode: "full",
-    showDownload: false,
-    theme: "dark",
-    getAudioInstance(instance) {
-      setAudioInstance(instance);
-    },
-  };
+function MusicPlayer(playerOptions, audioInstance) {
   return (
     <>
       {/* eslint-disable-next-line */}
-      <ReactJkMusicPlayer {...options} />
-
+      <ReactJkMusicPlayer {...playerOptions} />
+      {/* eslint-disable-next-line */}
       <button type="button" onClick={() => audioInstance.playPrev()}>
         Previous track
       </button>
+      {/* eslint-disable-next-line */}
       <button type="button" onClick={() => audioInstance.togglePlay()}>
         Play / Pause
       </button>
+      {/* eslint-disable-next-line */}
       <button type="button" onClick={() => audioInstance.playNext()}>
         Next track
       </button>
