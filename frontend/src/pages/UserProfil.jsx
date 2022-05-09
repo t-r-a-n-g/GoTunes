1982a4b89de0c348700ec103d7c2c46c0d7e84f4
norbert <norbert.madauss@gmail.com>
2022-05-09 14:48:22 +0200

    language toggle button

--- frontend/src/pages/UserProfil.jsx
+++ frontend/src/pages/UserProfil.jsx
@@ -1,11 +1,11 @@
+/* eslint-disable */
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import User from "@components/User";
 import CardPlaylists from "@components/Cards/CardPlaylists";
 import SettingsIcon from "@mui/icons-material/Settings";
-import Switch from "@mui/material/Switch";
-import Stack from "@mui/material/Stack";
-import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import i18n from "../i18nextConfig";
 
 // Fetch Playlists and display in Profil
 // Fetch Favourite Artists and display in Profil
@@ -15,6 +15,16 @@ import Typography from "@mui/material/Typography";
 
 export default function UserProfil() {
   const navigate = useNavigate();
+  const [checked, setChecked] = React.useState(
+    localStorage.getItem("languageToggle")
+  );
+
+  const handleChange = () => {
+    setChecked(!checked);
+    localStorage.setItem("languageToggle", checked);
+
+    checked === true ? i18n.changeLanguage("en") : i18n.changeLanguage("de");
+  };
 
   return (
     <div>
@@ -26,11 +36,12 @@ export default function UserProfil() {
           alignItems: "center",
         }}
       >
-        <Stack direction="row" alignItems="baseline">
-          <Typography>EN</Typography>
-          <Switch defaultChecked />
-          <Typography>DE</Typography>
-        </Stack>
+        <Button
+          onClick={handleChange}
+          sx={{ height: "50px", width: "50px", color: "text.primary" }}
+        >
+          {checked === true ? "DE" : "EN"}
+        </Button>
 
         <SettingsIcon
           sx={{ marginLeft: "20px" }}
